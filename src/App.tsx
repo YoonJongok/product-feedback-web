20b7d83558318b219a8707db1f03fd6b3382fc44
Jongok Yoon <yoonjongok1209@gmail.com>
2023-05-14 11:54:35 +0200

    svgr adapted for using svg as react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
+import { ReactComponent as Plus } from './assets/shared/icon-arrow-left.svg';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div>hi</div>;
+  return (
+    <div>
+      <Plus />
+    </div>
+  );
 }
 
 export default App;
